cdac59ae44c39f633dfc6972644cf4b6312bf2bb
Ralf Spöth <ralf.spoeth@profidata.com>
2023-11-25 16:10:08 +0100

    graphs removed

--- src/main/java/io/github/ralfspoeth/basix/graph/ComparableLabel.java
+++
@@ -1,8 +0,0 @@
-package io.github.ralfspoeth.basix.graph;
-
-public record ComparableLabel<T extends Comparable<? super T>>(T label) implements Comparable<ComparableLabel<T>> {
-    @Override
-    public int compareTo(ComparableLabel<T> o) {
-        return label.compareTo(o.label);
-    }
-}
